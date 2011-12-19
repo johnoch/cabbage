719552b907be9e945aa7881b6fba40c8e5c6f72e
rorywalsh@gmail.com <rorywalsh@gmail.com@3673a42d-6a86-3b09-a15b-10bc7187f823>
2011-12-19 15:21:33 +0000

    Build Script fro Windows. Usage: "BuildWin32.bat all" from MSVC Command Prompt

---
+++ Builds/VisualStudio2010/BuildWin32.bat
@@ -0,0 +1,31 @@
+echo off
+IF "%1"=="all" goto ALL
+IF "%1"=="standalone" goto STANDALONE
+IF "%1"=="plugins" goto PLUGINS
+
+:ALL
+echo Building All 
+msbuild CabbagePlugin.sln /t:Build /p:Configuration=Debug /p:DefineConst="Cabbage_Plugin_Synth"
+COPY Debug\CabbagePlugin.dll CabbagePluginSynth.dat
+msbuild CabbagePlugin.sln /t:Build /p:Configuration=Debug
+COPY Debug\CabbagePlugin.dll CabbagePluginEffect.dat
+msbuild Cabbage.sln /t:Build /p:Configuration=Debug /p:DefineConst="Cabbage_Plugin_Synth"
+COPY Debug\Cabbage.exe Cabbage.exe
+echo Pleae check current directory for Cabbage.exe, CabbagePluginSynth.dat and CabbagePluginEffect.dat. If any files are missing seomthing went wrong in the build process!  
+goto END
+
+:PLUGINS
+echo Building Plugins 
+msbuild CabbagePlugin.sln /t:Build /p:Configuration=Debug /p:DefineConst="Cabbage_Plugin_Synth"
+COPY Debug\CabbagePlugin.dll CabbagePluginSynth.dat
+goto END
+
+:STANDALONE
+echo Building Standalone 
+msbuild Cabbage.sln /t:Build /p:Configuration=Debug /p:DefineConstants="JucePlugin_IsSynth=1"
+COPY Debug\Cabbage.exe Cabbage.exe
+goto END
+
+:END
+echo Build complete
+:REM C:\MyDocuments\CabbageSVN\Builds\VisualStudio2010>msbuild CabbagePlugin.sln /t:ReBuild /p:Configuration=Debug /p:DefineConstants="JucePlugin_IsSynth=1"
