4c5d9c166752fa8d714453e4ab8c4958bbaa7d75
rorywalsh@gmail.com <rorywalsh@gmail.com@3673a42d-6a86-3b09-a15b-10bc7187f823>
2012-02-18 17:03:52 +0000

    Fix to let keyboard focused while playing other controls..

--- Source/Plugin/CabbagePluginEditor.cpp
+++ Source/Plugin/CabbagePluginEditor.cpp
@@ -298,9 +298,10 @@ void CabbagePluginAudioProcessorEditor::paint (Graphics& g)
 	for(int i=0;i<getFilter()->getGUILayoutCtrlsSize();i++){
 		if(getFilter()->getGUILayoutCtrls(i).getStringProp("type").equalsIgnoreCase("keyboard")){
 #ifdef Cabbage_Build_Standalone
-	//		layoutComps[i]->setWantsKeyboardFocus(true);
-	//		layoutComps[i]->grabKeyboardFocus();
-	//		layoutComps[i]->toFront(true);
+			//this lets controls keep focus even when you are palying the keyboard
+			layoutComps[i]->setWantsKeyboardFocus(true);
+			layoutComps[i]->grabKeyboardFocus();
+			layoutComps[i]->toFront(true);
 #endif
 		}
 	}
