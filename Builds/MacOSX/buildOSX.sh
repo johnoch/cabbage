d4b915695ad5177ac0c34c6aa9aa1beb593f35bf
Rory Walsh <rorywalsh@ear.ie>
2013-01-31 16:16:38 +0000

    updated for AU stuff...

--- Builds/MacOSX/buildOSX.sh
+++ Builds/MacOSX/buildOSX.sh
@@ -9,14 +9,14 @@ if [ $1 == "plugins" ]
 then
 	echo "Building Plugins Only"	
 	xcodebuild -project CabbagePlugin.xcodeproj/ GCC_PREPROCESSOR_DEFINITIONS="Cabbage_Plugin_Synth=1 USE_DOUBLE=1 MACOSX=1 Debug=1"
-	rm -rf ./build/Debug/Cabbage.app/Contents/CabbagePluginSynth.dat
-	cp -rf ./build/Debug/CabbagePlugin.component/ ./build/Debug/Cabbage.app/Contents/CabbagePluginSynth.dat
+	rm -rf ./build/Debug/Cabbage.app/Contents/CabbagePluginSynth.component
+	cp -rf ./build/Debug/CabbagePlugin.component/ ./build/Debug/Cabbage.app/Contents/CabbagePluginSynth.component
 	rm -rf ./build/Debug/CabbagePluginSynth.dat/CabbagePlugin.component
 
 	xcodebuild -project CabbagePlugin.xcodeproj/ GCC_PREPROCESSOR_DEFINITIONS="MACOSX=1 Debug=1 USE_DOUBLE=1"
-	rm -rf ./build/Debug/Cabbage.app/Contents/CabbagePluginEffect.dat
-	cp -rf ./build/Debug/CabbagePlugin.component/ ./build/Debug/Cabbage.app/Contents/CabbagePluginEffect.dat
-	rm -rf ./build/Debug/CabbagePluginEffect.dat/CabbagePlugin.component
+	rm -rf ./build/Debug/Cabbage.app/Contents/CabbagePluginEffect.component
+	cp -rf ./build/Debug/CabbagePlugin.component/ ./build/Debug/Cabbage.app/Contents/CabbagePluginEffect.component
+	rm -rf ./build/Debug/CabbagePluginEffect.component
 	rm -rf ~/Library/Audio/Plug-Ins/VST/CabbagePlugin.vst
 fi
 
@@ -26,12 +26,12 @@ then
 	xcodebuild -project Cabbage.xcodeproj/
 
 	xcodebuild -project CabbagePlugin.xcodeproj/ GCC_PREPROCESSOR_DEFINITIONS="Cabbage_Plugin_Synth=1 USE_DOUBLE=1 MACOSX=1 Debug=1"
-	cp -rf ./build/Debug/CabbagePlugin.component/ ./build/Debug/Cabbage.app/Contents/CabbagePluginSynth.dat
+	cp -rf ./build/Debug/CabbagePlugin.component/ ./build/Debug/Cabbage.app/Contents/CabbagePluginSynth.component
 	rm -rf ./build/Debug/CabbagePluginSynth.dat/CabbagePlugin.component
 	
 	xcodebuild -project CabbagePlugin.xcodeproj/ GCC_PREPROCESSOR_DEFINITIONS="MACOSX=1 Debug=1 USE_DOUBLE=1"
-	cp -rf ./build/Debug/CabbagePlugin.component/ ./build/Debug/Cabbage.app/Contents/CabbagePluginEffect.dat
-	rm -rf ./build/Debug/CabbagePluginEffect.dat/CabbagePlugin.component
+	cp -rf ./build/Debug/CabbagePlugin.component/ ./build/Debug/Cabbage.app/Contents/CabbagePluginEffect.component
+	rm -rf ./build/Debug/CabbagePluginEffect.component
 	rm -rf ~/Library/Audio/Plug-Ins/VST/CabbagePlugin.vst
 fi
 
