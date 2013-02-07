ca5b451b150d81826f5f5d479aba38c7f01062dc
Rory <rorywalsh@ear.ie>
2013-02-07 18:34:41 +0000

    fixed windows build problem with synths

--- Builds/VisualStudio2010/BuildWin32.bat
+++ Builds/VisualStudio2010/BuildWin32.bat
@@ -4,6 +4,8 @@ IF "%1"=="standalone" goto STANDALONE
 IF "%1"=="plugins" goto PLUGINS
 IF "%1"=="release" goto RELEASE
 
+REM http://social.msdn.microsoft.com/Forums/eu/msbuild/thread/8645667d-fba3-4f87-8606-d0aa6407ab1b
+
 :RELEASE
 echo Release Builds 
 msbuild CabbagePlugin.sln /t:ReBuild /p:Configuration=Debug
@@ -19,16 +21,16 @@ goto END
 echo Building All 
 msbuild CabbagePlugin.sln /t:ReBuild /p:Configuration=Debug
 COPY Debug\CabbagePlugin.dll CabbagePluginEffect.dat
-msbuild CabbagePlugin.sln /t:REBuild /p:Configuration=Debug /p:DefineConst="Cabbage_Plugin_Synth"
+msbuild CabbagePlugin.sln /t:REBuild /p:Configuration=Debug /p:DefineConst="Cabbage_Plugin_Synth=1"
 COPY Debug\CabbagePlugin.dll CabbagePluginSynth.dat
-msbuild Cabbage.sln /t:ReBuild /p:Configuration=Debug /p:DefineConst="Cabbage_Plugin_Synth"
+msbuild Cabbage.sln /t:ReBuild /p:Configuration=Debug /p:DefineConst="Cabbage_Plugin_Synth=1"
 COPY Debug\Cabbage.exe Cabbage.exe
 echo Pleae check current directory for Cabbage.exe, CabbagePluginSynth.dat and CabbagePluginEffect.dat. If any files are missing seomthing went wrong in the build process!  
 goto END
 
 :PLUGINS
 echo Building Plugins 
-msbuild CabbagePlugin.sln /t:Build /p:Configuration=Debug /p:DefineConst="Cabbage_Plugin_Synth"
+msbuild CabbagePlugin.sln /t:Build /p:Configuration=Debug /p:UserConstant="Cabbage_Plugin_Synth"
 COPY Debug\CabbagePlugin.dll CabbagePluginSynth.dat
 msbuild CabbagePlugin.sln /t:ReBuild /p:Configuration=Debug
 COPY Debug\CabbagePlugin.dll CabbagePluginEffect.dat
@@ -36,7 +38,7 @@ goto END
 
 :STANDALONE
 echo Building Standalone 
-msbuild Cabbage.sln /t:Build /p:Configuration=Debug /p:DefineConstants="JucePlugin_IsSynth=1"
+msbuild Cabbage.sln /t:Build /p:Configuration=Debug /p:DefineConstants="Cabbage_Plugin_Synth=1"
 COPY Debug\Cabbage.exe Cabbage.exe
 goto END
 
